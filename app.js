1cc309306dd82f10fd40bd1ec095f07a53fff4e6
Vakalapudi, Raviteja <Raviteja.Vakalapudi@viacomcontractor.com>
2017-02-05 15:11:55 -0500

    Added Favicon

--- app.js
+++ app.js
@@ -30,22 +30,4 @@ router.get("/contact",function(req,res){
 
 app.use("/",router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
