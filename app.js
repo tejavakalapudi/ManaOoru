e642bc975a7298436cb07fee96e13568c5973d7f
Vakalapudi, Raviteja <Raviteja.Vakalapudi@viacomcontractor.com>
2017-02-05 14:16:36 -0500

    deleted routes

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var about = require('./routes/about');
-var contact = require('./routes/contact');
+var router = express.Router();
+var path = __dirname + '/views/';
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,11 +15,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/about', about);
-app.use('/contact', contact);
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
